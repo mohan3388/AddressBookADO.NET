74d5c3ad6e6d478dd4796d1c066a91b8864e0950
mohan <sahumohan338@gmail.com>
2022-09-06 15:20:30 +0530

    [mohan]add: Insert Data in database

--- AddressBook/AddressBook/AddressBookRepo.cs
+++ AddressBook/AddressBook/AddressBookRepo.cs
@@ -11,7 +11,7 @@ namespace AddressBook
     public class AddressBookRepo
     {
         static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = AddressBookAdo; Integrated Security = SSPI";
-        static SqlConnection connection = new SqlConnection(connectionString);
+        SqlConnection connection = new SqlConnection(connectionString);
 
         public void EstablishConnection()
         {
@@ -39,6 +39,46 @@ namespace AddressBook
                 {
                     throw new CustomException(CustomException.ExceptionType.Connection_Failed, "Connection not found");
                 }
+            }
+        }
+        public string AddContact(Model obj)
+        {
+            try
+            {
+                
+                SqlCommand com = new SqlCommand("spAddNewPersons", connection);
+                com.CommandType = CommandType.StoredProcedure;
+
+                com.Parameters.AddWithValue("@FirstName", obj.FirstName);
+                com.Parameters.AddWithValue("@LastName", obj.LastName);
+                com.Parameters.AddWithValue("@Address", obj.Address);
+                com.Parameters.AddWithValue("@City", obj.City);
+                com.Parameters.AddWithValue("@State", obj.State);
+
+                com.Parameters.AddWithValue("@ZipCode", obj.ZipCode);
+                com.Parameters.AddWithValue("@PhoneNumber", obj.PhoneNumber);
+                com.Parameters.AddWithValue("@Email", obj.Email);
+
+
+                connection.Open();
+                int i = com.ExecuteNonQuery();
+                connection.Close();
+                if (i != 0)
+                {
+                    return "data Added";
+                }
+                else
+                {
+                    return "data not added";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close();
             }
         }
     }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,6 +7,39 @@
             AddressBookRepo repo = new AddressBookRepo();
             repo.EstablishConnection();
             repo.CloseConnection();
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("1. To check connection \n2. To Insert the Data in Data Base \n");
+                Console.WriteLine("Enter the Above Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        repo.EstablishConnection();
+                        repo.CloseConnection();
+                        break;
+                     case 2:
+                        Model empModel = new Model();
+                        //empModel.Id = 1;
+                        empModel.FirstName = "Mohan";
+                        empModel.LastName = "Sahu";
+                        empModel.Address = "Bera";
+                        empModel.City = "Bemetara";
+                        empModel.State = "CG";
+
+                        empModel.ZipCode = 491335;
+                        empModel.PhoneNumber = "7898625487";
+                        empModel.Email = "Mohan@12gmail.com";
+
+                        repo.AddContact(empModel);
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter the Correct option");
+                        break;
+
+                }
+            }
         }
     }
 }
