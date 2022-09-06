3c982df48df2d577697f67b29fe94e529c6fcfbd
mohan <sahumohan338@gmail.com>
2022-09-06 14:55:53 +0530

    [mohan]add: create connection in database

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBookRepo repo = new AddressBookRepo();
+            repo.EstablishConnection();
+            repo.CloseConnection();
+        }
+    }
+}
